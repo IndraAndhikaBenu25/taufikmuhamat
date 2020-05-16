5d4a00a88ee4bdc075d52bb3f10557af396198a0
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 21:57:27 +0800

    Create Jobsheet 7.cpp

---
+++ Jobsheet 7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"Nama     :Indra Andhika Benu"<<endl;
+	cout<<"Nim      :F1B019072"<<endl;
+	cout<<"Kelompok :14"<<endl;
+
+int nim [1];
+for (int i = 0; i<= 1; i++){
+cout<< "Masukkan nimke-= " << i+1;
+cin>>nim[i];
+}
+}
