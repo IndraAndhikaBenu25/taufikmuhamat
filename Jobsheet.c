2004633525718d54a78be91fedc3eb4f453b4135
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 22:03:33 +0800

    Create Jobsheet.c

---
+++ Jobsheet.c
@@ -0,0 +1,28 @@
+ #include <iostream>
+using namespace std;
+
+main (){
+	cout<<"Nama     :Indra Andhika Benu"<<endl;
+	cout<<"Nim      :F1B019072"<<endl;
+	cout<<"Kelompok :14"<<endl;
+
+	char matrik1 [3][3][3];
+	int i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j=0; j<3; j++)
+		{
+			cout << "Masukkan nilai matrik " << i << "," << j << " = ";
+			cin >> matrik1[i][j];
+		}
+	}
+	cout << endl;
+	cout<<"nilai matrik dengan array 3 dimensi = "<<endl;
+	for (i=0; i< 3; i++){
+		for (j = 0; j < 3; j++){
+
+			cout<<matrik1[i][j]<<" ";
+		}
+		cout << endl;
+	}
+}
