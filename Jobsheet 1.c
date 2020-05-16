e97510a8299dd48200c93f2a11c324eb3b84740d
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 20:44:58 +0800

    Create Jobsheet 1.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+int nim[9]= {10,9,8,7,2};
+printf("Nama 	 = Indra Nadhika Benu\n");
+printf("NIM 	 = F1B019172\n");
+printf("Kelompok = 14\n\n");
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+}
