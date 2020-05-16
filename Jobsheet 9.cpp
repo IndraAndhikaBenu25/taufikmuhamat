9e3b27a99cfe81c74b008ee62b85559698efb3df
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 22:02:02 +0800

    Create Jobsheet 9.cpp

---
+++ Jobsheet 9.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+using namespace std;
+int main(){
+	cout<<"Nama     :Indra Andhika Benu"<<endl;
+	cout<<"Nim      :F1B019072"<<endl;
+	cout<<"Kelompok :14"<<endl;
+
+
+int i;
+char nama[5]={'A','I','R',};
+char nama2[6]="AIR";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
+
