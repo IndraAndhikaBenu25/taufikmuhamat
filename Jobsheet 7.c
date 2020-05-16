62ca79ec250e30df433b61c5010c73bef333402a
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 21:56:40 +0800

    Create Jobsheet 7.c

---
+++ Jobsheet 7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+printf ("Nama     : Indra Andhika Benu\n");
+printf ("Nim      : F1B019072\n ");
+printf ("Kelompok : 14\n");
+
+int nim [1], i;
+for (i = 0; i<= 1; i++){
+printf ("Masukkan nimke-%i= ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
