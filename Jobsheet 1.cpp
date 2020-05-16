42a17cb02e0562a9f5efdb7f00b26600d220a808
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 20:41:25 +0800

    Create Jobsheet 1.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nim[5]= {10,9,8,7,2};
+	cout<<"Nama       = Indra Andhika Benu\n";
+	cout<<"NIM        = F1B0190872\n";
+	cout<<"Kelompok   = 14\n\n";
+
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+}
