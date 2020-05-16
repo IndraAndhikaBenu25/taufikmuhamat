15f7d6faae30989f3b4ef1c1a29b6dd5a7708a58
IndraAndhikaBenu25 <64628482+IndraAndhikaBenu25@users.noreply.github.com>
2020-05-16 21:55:49 +0800

    Create Jobsheet 6.cpp

---
+++ Jobsheet 6.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"Nama     :Indra Andhika Benu"<<endl;
+	cout<<"Nim      :F1B019072"<<endl;
+	cout<<"Kelompok :14"<<endl;
+
+
+int x,y, mtrx [5][5];
+cout<< "TRANSPOSE MATRIX" <<endl;
+cout<< "Masukkan jumlahbaris = ";
+cin>> x;
+cout<< "Masukkan jumlahkolom = ";
+cin>> y;
+cout<<endl;
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<< "Masukkan nilai index " <<i<< "," << j << " = ";
+cin>>mtrx [i][j];
+}
+}
+cout<<endl;
+for (int i = 0; i< x; i++){
+for (int j = 0; j < y; j++){
+cout<<mtrx [i][j] <<"  ";
+}
+cout<<endl<<endl;
+}
+cout<<endl;
+for (int i = 0; i< y; i++){
+for (int j = 0; j < x; j++){
+cout<<mtrx [j][i] <<"  ";
+}
+cout<<endl<<endl;
+}
+}
